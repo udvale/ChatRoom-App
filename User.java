c8dc0d717cdb58f4335c490441130e907778eca1
Alexandrialexie <98338885+Alexandrialexie@users.noreply.github.com>
2024-03-27 20:15:28 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,19 @@
+package FinalProject;
+
+public class User {
+	private Integer userID;
+	private String userName;
+	
+	public User(int initUserID, String initUserName) {
+		userID = initUserID;
+		userName = initUserName;
+	}
+	
+	public Integer getID() {
+		return userID;
+	}
+	
+	public String getName() {
+		return userName;
+	}
+}
